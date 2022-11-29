5fd9ca6492d887074435fe78453ad363db07c760
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-11-30 03:25:26 +0600

    installed firebase and used environment variable to hide firebaseConfig

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
     const { register, handleSubmit } = useForm();
-    const [data, setData] = useState("");
-
+    const handleLogin = data => {
+        console.log(data);
+    }
 
     return (
         <div className='h-[800px] flex flex-col justify-center items-center'>
             <h2 className='text-3xl flex justify-center underline mb-5'>Login</h2>
-            <div className=''>
-                <form className='bg-white p-4' onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+            <div className='bg-white pb-5'>
+                <form className='text-center bg-white w-96 p-5 -me-5' onSubmit={handleSubmit(handleLogin)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Email</span>
@@ -22,15 +24,18 @@ const Login = () => {
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text">Password</span>
                         </label>
                         <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
                         <input />
                     </div>
-
-                    <p>{data}</p>
-                    <input type="submit" />
+                    <input className='btn btn-dark text-white w-80' value="Login" type="submit" />
                 </form>
+                <p className='text-center'>New to this site? Then <Link className='text-primary' to={'/register'}>create account</Link> .</p>
+                <div className="divider px-5">OR</div>
+                <div className='flex justify-center items-center'>
+                    <button className='btn btn-dark'>SIGN IN WITH GOOGLE</button>
+                </div>
             </div>
 
         </div>
